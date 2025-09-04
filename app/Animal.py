1ac496bdcf0fc192a15c0712c8bf9de8634dd23c
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-09-04 14:49:58 +0200

    Solution #2

--- app/Animal.py
+++ app/Animal.py
@@ -1,21 +1,26 @@
 class Animal:
     alive = list()
 
-    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
-    def __repr__(self) -> str:
-        return f'Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}'
+    def __repr__(self) -> str :
+        return (f'Name: {self.name},'
+                f' Health: {self.health},'
+                f' Hidden: {self.hidden}')
 
-    def die(self):
+    def die(self) -> None:
         Animal.alive.remove(self)
 
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 
